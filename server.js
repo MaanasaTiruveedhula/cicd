41955cb26b3dc8610ca7bc99413ca0fe55d2af93
MaanasaTiruveedhula <47042666+MaanasaTiruveedhula@users.noreply.github.com>
2024-06-25 23:20:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Hello world change in the code deploy ")
+    res.end("Hello world, second build happening now!")
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
